8881de93b6a82024507831706be16f0d3c072615
Jacapo Cirrone <cirrone@nyu.edu>
2013-11-06 01:41:31 +0000

    Adding DM

---
+++ DistDB/src/java/Server/DM7.java
@@ -0,0 +1,17 @@
+/**
+ * This class implements web service logic Server side
+ */
+
+package Server;
+import java.util.*;
+import java.io.*;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import java.net.MalformedURLException;
+import json.*;
+import json.JSONArray.*;
+
+@WebService()
+public class DM7 extends DM{
+
+}
